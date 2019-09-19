2058e2091918a10bf36f36e531bcdc51af12b38e
KienLe <kien.le.van.89@gmail.com>
2019-09-19 10:47:44 +0700

    First

---
+++ dotfile.sh
@@ -0,0 +1,25 @@
+## -------------- My Dotfile -------------- ##
+cd
+
+## Set up Vim
+
+VIMRC_FOLDER="$HOME/myvimrc/"
+if [[ -d $VIMRC_FOLDER ]]; then
+    if [[ -d  "$VIMRC_FOLDER/.git" ]]; then
+	# There's myvimrc repo already, pull the repo
+	cd $VIMRC_FOLDER
+	git pull origin master
+	cd
+    else
+	# There's myvimrc folder, but it's not the my repo, pull the repo
+	rm -r $VIMRC_FOLDER
+	git clone git@github.com:xZixz/myvimrc.git
+    fi
+else
+    # There's no myvimrc folder, pull the repo
+    git clone git@github.com:xZixz/myvimrc.git
+fi
+
+# Soft link myvimrc/.vimrc
+rm .vimrc
+ln -s myvimrc/.vimrc .vimrc
